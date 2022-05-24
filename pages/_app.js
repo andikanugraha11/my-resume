b1f800528795a35f24ea877bc75cee01f1262ae8
Andika Nugraha <m.andika.nugraha@gmail.com>
2022-05-24 23:49:36 +0700

    update style

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
-import '../styles/globals.css'
+import '../styles/reset.css';
+import '../styles/vendor/animations.css';
+import '../styles/vendor/perfect-scrollbar.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+// import '../styles/owl.carousel.css';
+import '../styles/vendor/perfect-scrollbar.css';
+import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
